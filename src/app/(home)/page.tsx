81d02b47089b3617ab31c2833d1840ec859781ad
fernand0cruz <cruzfernando.dev@gmail.com>
2023-11-22 18:26:38 -0300

    feat: add ProductList and ProductItem components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,31 @@
+import { prismaClient } from "@/lib/prisma"
 import Categories from "./components/categories"
+import ProductHorizontalList from "./components/product-list"
 
-export default function Home() {
+export default async function Home() {
+  const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      }
+    }
+  })
   return (
-    <div className="p-5">
-      <img className="h-auto w-full rounded-lg" src="https://placehold.co/600x200/292929/white?text=Banner+Promo%C3%A7%C3%A3o" alt="" />
-      <div className="mt-6">
+    <div >
+      <div className="m-5">
+        <img
+          className="h-auto w-full rounded-lg"
+          src="https://placehold.co/600x200/292929/white?text=Banner+Promo%C3%A7%C3%A3o"
+          alt=""
+        />
+      </div>
+
+      <div className="mx-5">
         <Categories />
+      </div>
+
+      <div className="my-5">
+        <ProductHorizontalList products={deals} />
       </div>
     </div>
   )
---
+++ src/components/ui/pruduct-item.tsx
@@ -0,0 +1,50 @@
+import { ProductWithTotalPrice } from "@/helpers/product";
+import { Product } from "@prisma/client";
+import { Badge } from "./badge";
+import { ArrowDownIcon } from "lucide-react";
+interface ProductItemProps {
+    product: ProductWithTotalPrice
+}
+
+const ProductItem = ({ product }: ProductItemProps) => {
+    return (
+        <div className="flex flex-col max-w-[156px] gap-4">
+            <div className="bg-[#292929] rounded-lg w-[156px] h-[170px] flex items-center justify-center relative">
+                <img
+                    src={product.imgUrls[0] + "?text=" + product.name + " img"}
+                    alt={product.name}
+                    className="w-auto h-auto max-w-[80%] max-h-[70%]:"
+                    style={{
+                        objectFit: "contain"
+                    }}
+                />
+                {
+                    product.discountPercentage > 0 && (
+                        <Badge className="absolute left-3 top-3 px-2 py-[2px]">
+                            <ArrowDownIcon size={14}/> {product.discountPercentage}%
+                        </Badge>
+                    )
+                }
+            </div>
+
+
+            <div>
+                <p className="text-sm overflow-hidden whitespace-nowrap text-ellipsis">{product.name}</p>
+                <div className="flex items-center gap-2">
+                    {
+                        product.discountPercentage > 0 ? (
+                            <>
+                                <p className="font-semibold texr-sm">R$ {product.totalPrice.toFixed(2)}</p>
+                                <p className="opacity-75 line-through text-xs">R$ {product.basePrice.toFixed(2)}</p>
+                            </>
+                        ) : (
+                            <p className="font-semibold texr-sm">R$ {product.basePrice.toFixed(2)}</p>
+                        )
+                    }
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ProductItem;
