fd238b7b002209af30b32977afc151d878f0e9a4
fernand0cruz <cruzfernando.dev@gmail.com>
2023-11-22 17:54:23 -0300

    feat: add ProductItem components

---
+++ src/app/(home)/components/product-list.tsx
@@ -0,0 +1,19 @@
+import { Product } from "@prisma/client"
+import ProductItem from "@/components/ui/pruduct-item"
+
+interface ProductListProps {
+  products: Product[]
+}
+
+const productHorizontalList = ( {products}: ProductListProps ) => {
+  return (
+    <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
+      {
+        products.map(product => <ProductItem key={product.id} product={product}/>)
+      }
+    </div>
+  )
+}
+
+export default productHorizontalList
+
