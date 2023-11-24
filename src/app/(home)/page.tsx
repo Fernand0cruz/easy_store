fd3a797360d0b5669c51fbd6e76f1775035f8a45
fernand0cruz <cruzfernando.dev@gmail.com>
2023-11-24 12:48:17 -0300

    feat: add second banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,6 @@
 import { prismaClient } from "@/lib/prisma"
 import Categories from "./components/categories"
-import ProductHorizontalList from "./components/product-list"
+import ProductList from "./components/product-list"
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
@@ -12,10 +12,11 @@ export default async function Home() {
   })
   return (
     <div >
+
       <div className="m-5">
         <img
           className="h-auto w-full rounded-lg"
-          src="https://placehold.co/600x200/292929/white?text=Banner+Promo%C3%A7%C3%A3o"
+          src="https://placehold.co/600x300/292929/white?text=Banner+Promo%C3%A7%C3%A3o+1"
           alt=""
         />
       </div>
@@ -25,8 +26,18 @@ export default async function Home() {
       </div>
 
       <div className="my-5">
-        <ProductHorizontalList products={deals} />
+        <p className="pl-5 font-bold uppercase mb-3">Ofertas</p>
+        <ProductList products={deals} />
       </div>
+
+      <div className="m-5">
+        <img
+          className="h-auto w-full rounded-lg"
+          src="https://placehold.co/600x300/292929/white?text=Banner+Promo%C3%A7%C3%A3o+2"
+          alt=""
+        />
+      </div>
+      
     </div>
   )
 }
