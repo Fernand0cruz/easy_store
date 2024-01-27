164af055e816170e17689f57b32f9c8a818e7893
fernand0cruz <cruzfernando.dev@gmail.com>
2024-01-27 14:32:08 -0300

    feat: add Footer component

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return ( 
+        <div className="bg-accent px-8 py-4 opacity-75">
+            Copyright (C) 2023-2024 <span className="font-semibold"><a href="https://fernandow.vercel.app/">Fernand0W</a></span> 
+        </div>
+     );
+}
+ 
+export default Footer
+;
