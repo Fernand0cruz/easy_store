6b154c7e0d62b87980a79a8ad541d607177e4024
fernand0cruz <cruzfernando.dev@gmail.com>
2024-01-28 14:04:43 -0300

    feat: add category itens

---
+++ src/app/catalog/components/category-item.tsx
@@ -0,0 +1,19 @@
+import { Category } from "@prisma/client"
+
+interface CategoryItensProps {
+    category: Category
+}
+const CategoryItens = ({ category }: CategoryItensProps) => {
+    return (
+        <div className="flex flex-col">
+            <div className="w-full h-[150px] flex justify-center items-center bg-gradient-to-r from-[#5033c3] to-[rgba(80, 51, 195, 0.20)] rounded-tl-lg rounded-tr-lg">
+                <img src={category.imgUrl} alt={category.name} width={0} height={0} sizes="100vw" className="h-auto max-h-[70%] w-auto max-w-[80%]" style={{ objectFit: "contain" }} />
+            </div>
+            <div className="bg-accent py-3 rounded-br-lg rounded-bl-lg">
+                <p className="text-center text-sm font-semibold">{category.name}</p>
+            </div>
+        </div>
+    );
+}
+
+export default CategoryItens;
