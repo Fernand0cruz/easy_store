5c36458e38cc3ae65161f6abd3df6d7c2c1aafeb
fernand0cruz <cruzfernando.dev@gmail.com>
2024-02-03 20:13:16 -0300

    feat: add recommended products title to product datails page

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -3,20 +3,21 @@ import ProductImage from "./components/product-images"
 import ProductInfo from "./components/product-inf"
 import { computeProductTotalPrice } from "@/helpers/product"
 import ProductList from "@/components/ui/product-list"
+import SectionTittle from "@/components/ui/section-tittle"
 interface ProductDetailsPageProps {
-        params: {
-            slug: string
-        }
+    params: {
+        slug: string
     }
+}
 
-const ProductDetailsPage = async ({params: { slug }}: ProductDetailsPageProps) => {
+const ProductDetailsPage = async ({ params: { slug } }: ProductDetailsPageProps) => {
     const product = await prismaClient.product.findFirst({
         where: {
             slug: slug
         },
         include: {
             category: {
-                include:{
+                include: {
                     products: {
                         where: {
                             slug: {
@@ -29,14 +30,17 @@ const ProductDetailsPage = async ({params: { slug }}: ProductDetailsPageProps) =
         }
 
     })
-    if(!product) return null
-    return ( 
+    if (!product) return null
+    return (
         <div className="flex flex-col gap-8 pb-8">
-            <ProductImage  imageUrls={product.imgUrls} name={product.name}/>
-            <ProductInfo product={computeProductTotalPrice(product)}/>
-            <ProductList products={product.category.products}/>
+            <ProductImage imageUrls={product.imgUrls} name={product.name} />
+            <ProductInfo product={computeProductTotalPrice(product)} />
+            <div>
+                <SectionTittle>Produtos semelhantes</SectionTittle>
+                <ProductList products={product.category.products} />
+            </div>
         </div>
-     );
+    );
 }
 
 export default ProductDetailsPage;
