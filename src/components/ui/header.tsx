27f927f8aa050e82d2f4c4b245e5532ab048520d
fernand0cruz <cruzfernando.dev@gmail.com>
2024-03-07 20:03:49 -0300

    feat: add cart quantity indicator to header

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -9,10 +9,16 @@ import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
 import { Separator } from "./separator";
 import Link from "next/link";
 import Cart from "./cart";
+import { useContext } from "react";
+import { CartContext } from "@/providers/cart";
 
 const Header = () => {
 
     const { status, data } = useSession()
+
+    const { products } = useContext(CartContext)
+
+    const cartQuantityItems = products.length
 
     const handleLoginClick = async () => {
         await signIn()
@@ -101,12 +107,17 @@ const Header = () => {
 
             <Sheet>
                 <SheetTrigger asChild>
-                    <Button size="icon" variant="outline">
+                    <Button size="icon" variant="outline" className="relative">
+                        {cartQuantityItems > 0 && (
+                            <span className="absolute right-[calc(-1.25rem/2)] top-[calc(-1.25rem/2)] flex h-6 w-6 items-center justify-center rounded-lg bg-primary text-sm font-bold">
+                                {cartQuantityItems}
+                            </span>
+                        )}
                         <ShoppingCartIcon />
                     </Button>
                 </SheetTrigger>
                 <SheetContent className="w-[350px]">
-                    <Cart/>
+                    <Cart />
                 </SheetContent>
             </Sheet>
         </Card>
