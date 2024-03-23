914cf24b8e3c75a7a41fd2c3b7c114182a944efb
fernand0cruz <cruzfernando.dev@gmail.com>
2024-03-23 13:47:06 -0300

    style: remove unnecessary class in SectionTittle component

--- src/components/ui/section-tittle.tsx
+++ src/components/ui/section-tittle.tsx
@@ -2,7 +2,7 @@ import { ComponentProps } from "react";
 
 const SectionTittle = ( {children, ...props}: ComponentProps<"p"> ) => {
     return (
-        <p className="pl-5 font-bold uppercase mb-3" {...props}>{ children }</p>
+        <p className="font-bold uppercase mb-3" {...props}>{ children }</p>
     );
 }
 
