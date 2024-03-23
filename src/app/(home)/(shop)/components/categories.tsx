a066442b7bb3772a582ca8c0ea5ce90d234327e3
fernand0cruz <cruzfernando.dev@gmail.com>
2024-03-23 13:13:27 -0300

    feat: update category grid  responsability

--- src/app/(home)/(shop)/components/categories.tsx
+++ src/app/(home)/(shop)/components/categories.tsx
@@ -4,7 +4,7 @@ import CategoryItem from "./category-item"
 const categories = async () => {
     const categories = await prismaClient.category.findMany({})
     return (
-        <div className="grid grid-cols-2 gap-x-2 gap-y-2">
+        <div className="grid grid-cols-2 gap-x-2 gap-y-2 lg:grid-cols-6">
             {
                 categories.map ((category) => (
                     <CategoryItem key={category.id} category={category}/>
