25e0330608f8260e1aa8bc5526dff5a3e7cdf8d0
fernand0cruz <cruzfernando.dev@gmail.com>
2024-03-25 18:50:23 -0300

    style: update home/shop page layout

--- src/app/(home)/(shop)/page.tsx
+++ src/app/(home)/(shop)/page.tsx
@@ -36,52 +36,53 @@ export default async function Home() {
   })
   return (
     <div className="mx-auto max-w-[1920px] ">
-      <div className="mb-5">
+      <div className="my-5">
         <Link href={"offers"}>
           <PromotionalBanner src={"/banner_1.png"} alt="banner_1.png" />
         </Link>
       </div>
-      
-      <div>
-        <Categories />
-      </div>
+      <div className="mx-5">
+        <div>
+          <Categories />
+        </div>
 
-      <div className="my-5">
-        <SectionTittle>Ofertas</SectionTittle>
-        <ProductList products={deals} />
-      </div>
+        <div className="my-5">
+          <SectionTittle>Ofertas</SectionTittle>
+          <ProductList products={deals} />
+        </div>
 
-      <div>
-        <Link href={"/category/keyboards"}>
-          <PromotionalBanner src={"/banner_2.png"} alt="banner_2.png" />
-        </Link>
-      </div>
+        <div>
+          <Link href={"/category/keyboards"}>
+            <PromotionalBanner src={"/banner_2.png"} alt="banner_2.png" />
+          </Link>
+        </div>
 
-      <div className="my-5">
-        <SectionTittle>Teclado</SectionTittle>
-        <ProductList products={keyboards} />
-      </div>
+        <div className="my-5">
+          <SectionTittle>Teclado</SectionTittle>
+          <ProductList products={keyboards} />
+        </div>
 
-      <div>
-        <Link href={"/category/mouses"}>
-          <PromotionalBanner src={"/banner_3.png"} alt="banner_3.png" />
-        </Link>
-      </div>
+        <div>
+          <Link href={"/category/mouses"}>
+            <PromotionalBanner src={"/banner_3.png"} alt="banner_3.png" />
+          </Link>
+        </div>
 
-      <div className="my-5">
-        <SectionTittle>Mouse</SectionTittle>
-        <ProductList products={mouses} />
-      </div>
+        <div className="my-5">
+          <SectionTittle>Mouse</SectionTittle>
+          <ProductList products={mouses} />
+        </div>
 
-      <div>
-        <Link href={"/category/headphones"}>
-          <PromotionalBanner src={"/banner_4.png"} alt="banner_4.png" />
-        </Link>
-      </div>
+        <div>
+          <Link href={"/category/headphones"}>
+            <PromotionalBanner src={"/banner_4.png"} alt="banner_4.png" />
+          </Link>
+        </div>
 
-      <div className="my-5">
-        <SectionTittle>Fones</SectionTittle>
-        <ProductList products={headphones} />
+        <div className="my-5">
+          <SectionTittle>Fones</SectionTittle>
+          <ProductList products={headphones} />
+        </div>
       </div>
     </div>
   )
