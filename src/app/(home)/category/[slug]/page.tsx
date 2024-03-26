6230fc328a8f32720826ebe57c3b57e3682b3168
fernand0cruz <cruzfernando.dev@gmail.com>
2024-03-26 08:23:35 -0300

    style: refactor category page layout

--- src/app/(home)/category/[slug]/page.tsx
+++ src/app/(home)/category/[slug]/page.tsx
@@ -14,21 +14,23 @@ const CategoryProducts = async ({ params }: any) => {
         }
     })
     return (
-        <div className="p-5 flex flex-col gap-8">
+        <div className="p-5 flex flex-col gap-5 max-w-[1920px] m-auto">
             <Badge className="gap-1 text-base uppercase border-primary py-[0.365rem] border-2 w-fit" variant={"outline"}>
                 {CATEGORY_ICON[params.slug as keyof typeof CATEGORY_ICON]}
                 {category?.name}
             </Badge>
-            <div className="grid grid-cols-2 gap-8">
+            <div className="flex flex-wrap gap-5 justify-between">
                 {
                     category?.products.map((product) => (
-                        <ProductItem
-                            key={product.id}
-                            product={{
-                                ...product,
-                                totalPrice: computeProductTotalPrice(product),
-                            }}
-                        />
+                        <div key={product.id} className="w-[170px] lg:w-[200px] lg:min-w-[200px]">
+                            <ProductItem
+                                key={product.id}
+                                product={{
+                                    ...product,
+                                    totalPrice: computeProductTotalPrice(product),
+                                }}
+                            />
+                        </div>
                     ))
                 }
             </div>
