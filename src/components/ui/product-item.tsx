adb76555c45f9ef56976b8b64f01e1ce9cf9353d
fernand0cruz <cruzfernando.dev@gmail.com>
2024-06-19 00:24:46 -0300

    refactor: optimize product item component and product list

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -3,15 +3,16 @@ import { Badge } from "./badge";
 import { ArrowDownIcon } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
+
 interface ProductItemProps {
     product: ProductWithTotalPrice
 }
 
 const ProductItem = ({ product }: ProductItemProps) => {
     return (
-        <Link href={`/product/${product.slug}`} className="flex">
-            <div className="flex flex-col w-full gap-4 min-w-[160px]">
-                <div className="bg-[#292929] rounded-lg h-[170px] w-full flex items-center justify-center relative lg:h-[200px] lg:w-[200px]">
+        <Link href={`/product/${product.slug}`} className="flex" aria-label={`View details for ${product.name}`}>
+            <div className="flex flex-col gap-2 w-full min-w-[170px]">
+                <div className="rounded-lg border hover:bg-accent h-[185px] w-full flex items-center justify-center relative">
                     <Image
                         src={product.imgUrls[0]}
                         alt={product.name}
@@ -21,6 +22,7 @@ const ProductItem = ({ product }: ProductItemProps) => {
                         className="w-auto h-auto max-w-[80%] max-h-[70%] object-contain"
                         loading="lazy"
                     />
+                    
                     {
                         product.discountPercentage > 0 && (
                             <Badge className="absolute left-3 top-3 px-2 py-[2px]">
@@ -28,27 +30,27 @@ const ProductItem = ({ product }: ProductItemProps) => {
                             </Badge>
                         )
                     }
+
                 </div>
-
-
                 <div>
                     <p className="text-sm overflow-hidden whitespace-nowrap text-ellipsis">{product.name}</p>
                     <div className="flex items-center gap-2">
+                        
                         {
                             product.discountPercentage > 0 ? (
                                 <>
-                                    <p className="font-semibold texr-sm">R${product.totalPrice.toFixed(2)}</p>
+                                    <p className="font-semibold text-sm">R${product.totalPrice.toFixed(2)}</p>
                                     <p className="opacity-75 line-through text-[.8rem]">R${product.basePrice.toFixed(2)}</p>
                                 </>
                             ) : (
-                                <p className="font-semibold texr-sm">R$ {product.basePrice.toFixed(2)}</p>
+                                <p className="font-semibold text-sm">R$ {product.basePrice.toFixed(2)}</p>
                             )
                         }
+
                     </div>
                 </div>
             </div>
         </Link>
     );
 }
-
 export default ProductItem;
--- src/components/ui/product-list.tsx
+++ src/components/ui/product-list.tsx
@@ -1,27 +1,70 @@
-import { Product } from "@prisma/client"
-import ProductItem from "@/components/ui/product-item"
-import { computeProductTotalPrice } from "@/helpers/product"
+import { Product } from "@prisma/client";
+import ProductItem from "@/components/ui/product-item";
+import { computeProductTotalPrice } from "@/helpers/product";
 
 interface ProductListProps {
-  products: Product[]
+  products: Product[];
 }
 
-const productHorizontalList = ({ products }: ProductListProps) => {
+const ProductHorizontalList = ({ products }: ProductListProps) => {
+
+  // Função auxiliar para renderizar uma lista de produtos com um comprimento especificado
+  const renderProductList = (products: Product[], length: number) => {
+    // Verifica se há produtos disponíveis
+    if (!products || products.length === 0) {
+      return <div>Não há produtos disponíveis</div>; // Retorna uma mensagem se não houver produtos disponíveis
+    }
+
+    // Faz um map sobre o array de produtos e renderiza cada produto até o comprimento especificado
+    return products.slice(0, length).map((product) => (
+      <div key={product.id} className="w-[33.3%] md:w-[25%] lg:w-[16.6%]">
+        <ProductItem
+          product={{
+            ...product,
+            totalPrice: computeProductTotalPrice(product), // Calcula e inclui o preço total para cada produto
+          }}
+        />
+      </div>
+    ));
+  };
+
   return (
-    <div className="flex w-full gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-      {
-        products.map(product =>
-          <div key={product.id} className="w-[170px] lg:w-[200px] lg:min-w-[200px]">
-            <ProductItem product={{
-              ...product,
-              totalPrice: computeProductTotalPrice(product),
-            }} />
-          </div>
-        )
-      }
+    <div >
+      <div className="flex w-full gap-2 overflow-x-auto sm:hidden">
+
+        {
+          products.map(product =>
+            <div key={product.id} className="w-[170px] lg:w-[200px] lg:min-w-[200px]">
+              <ProductItem product={{
+                ...product,
+                totalPrice: computeProductTotalPrice(product),
+              }} />
+            </div>
+          )
+        }
+
+      </div>
+      {/* Visualização para tablets pequenos: layout flexível */}
+      <div className="hidden sm:flex gap-2 md:hidden">
+
+        {renderProductList(products, 3)} {/* Renderiza até 3 produtos */}
+
+      </div>
+
+      {/* Visualização para tablets maiores: layout flexível */}
+      <div className="hidden md:flex gap-2 lg:hidden">
+
+        {renderProductList(products, 4)} {/* Renderiza até 4 produtos */}
+
+      </div>
+
+      {/* Visualização para desktops: layout flexível */}
+      <div className="hidden lg:flex gap-2">
+
+        {renderProductList(products, 6)} {/* Renderiza até 6 produtos */}
+
+      </div>
     </div>
-  )
-}
-
-export default productHorizontalList
-
+  );
+};
+export default ProductHorizontalList;
