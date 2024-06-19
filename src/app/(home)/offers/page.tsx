3b765a431d79b4e41c77175d5ff05797d24e2839
fernand0cruz <cruzfernando.dev@gmail.com>
2024-06-19 00:59:51 -0300

    refactor: optimize category page and documentation added

--- src/app/(home)/offers/page.tsx
+++ src/app/(home)/offers/page.tsx
@@ -1,142 +1,71 @@
 import { Badge } from "@/components/ui/badge";
-import { HeadphonesIcon, KeyboardIcon, Monitor, MouseIcon, MousePointerSquare, SpeakerIcon, StarIcon } from "lucide-react";
+import {
+    ArrowRightFromLineIcon,
+    HeadphonesIcon,
+    KeyboardIcon,
+    Monitor,
+    MouseIcon,
+    MousePointerSquare,
+    SpeakerIcon
+} from "lucide-react";
 import PromotionalBanner from "../(shop)/components/promotional-banner";
 import ProductList from "@/components/ui/product-list";
 import { prismaClient } from "@/lib/prisma";
+import { Card } from "@/components/ui/card";
+import Link from "next/link";
+import SectionTittle from "@/components/ui/section-tittle";
+
+// Definição das categorias com seus respectivos ícones e rótulos
+const categories = [
+    { slug: "mouses", icon: MouseIcon, label: "Mouses em Promoção" },
+    { slug: "keyboards", icon: KeyboardIcon, label: "Teclados em Promoção" },
+    { slug: "headphones", icon: HeadphonesIcon, label: "Headphones em Promoção" },
+    { slug: "mousepads", icon: MousePointerSquare, label: "Mousepads em Promoção" },
+    { slug: "monitors", icon: Monitor, label: "Monitores em Promoção" },
+    { slug: "speakers", icon: SpeakerIcon, label: "Speakers em Promoção" },
+];
 
 export default async function Offers() {
-    const mouses = await prismaClient.product.findMany({
-        where: {
-            category: {
-                slug: "mouses",
-            },
-            discountPercentage: {
-                gt: 0,
-            },
-        },
-    });
-    const keyboards = await prismaClient.product.findMany({
-        where: {
-            category: {
-                slug: "keyboards",
-            },
-            discountPercentage: {
-                gt: 0,
-            },
-        },
-    });
-    const headphones = await prismaClient.product.findMany({
-        where: {
-            category: {
-                slug: "headphones",
-            },
-            discountPercentage: {
-                gt: 0,
-            },
-        },
-    });
-    const mousepads = await prismaClient.product.findMany({
-        where: {
-            category: {
-                slug: "mousepads",
-            },
-            discountPercentage: {
-                gt: 0,
-            },
-        },
-    });
-    const monitors = await prismaClient.product.findMany({
-        where: {
-            category: {
-                slug: "monitors",
-            },
-            discountPercentage: {
-                gt: 0,
-            },
-        },
-    });
-    const speakers = await prismaClient.product.findMany({
-        where: {
-            category: {
-                slug: "speakers",
-            },
-            discountPercentage: {
-                gt: 0,
-            },
-        },
-    });
+    // Busca produtos com desconto de cada categoria
+    const categoryProducts = await Promise.all(
+        categories.map(category =>
+            prismaClient.product.findMany({
+                where: {
+                    category: {
+                        slug: category.slug,
+                    },
+                    discountPercentage: {
+                        gt: 0, // Filtra produtos com desconto maior que 0
+                    },
+                },
+            }).then(products => ({ ...category, products }))
+        )
+    );
+
     return (
-        <div className="flex flex-col gap-8 mb-5 max-w-[1920px] m-auto">
-            <div className="mt-5">
-                <PromotionalBanner src={"/banner_1.png"} alt="" />
-            </div>
-            <div className="flex flex-col gap-5 mx-5">
-                {
-                    mouses.length > 0 && (
-                        <div className="flex flex-col gap-5">
-                            <Badge className="gap-1 text-base uppercase border-primary py-[0.365rem] border-2 w-fit" variant={"outline"}>
-                                <MouseIcon />
-                                Mouses em Promoção
-                            </Badge>
-                            <ProductList products={mouses} />
-                        </div>
+        <div className="flex flex-col gap-5 mb-5 max-w-screen-xl mx-auto">
+            <PromotionalBanner src={"/banner_1.png"} alt="Promoção" />
+            <div className="flex flex-col gap-5">
+                {/* Itera sobre cada categoria e renderiza os produtos */}
+                {categoryProducts.map(({ slug, icon: Icon, label, products }) => (
+                    products.length > 0 && (
+                        <Card key={slug} className="flex flex-col gap-5 py-4 px-2">
+                            <div className="flex justify-between">
+                                <div className="flex gap-2">
+                                    <Icon />
+                                    <SectionTittle>
+                                        {label}
+                                    </SectionTittle>
+                                </div>
+                                <Link href={`/category/${slug}`} className="hidden sm:flex">
+                                    <p className="uppercase font-bold flex">ver mais<ArrowRightFromLineIcon /></p>
+                                </Link>
+                            </div>
+                            <ProductList products={products} />
+                        </Card>
                     )
-                }
-                {
-                    keyboards.length > 0 && (
-                        <div className="flex flex-col gap-5">
-                            <Badge className="gap-1 text-base uppercase border-primary py-[0.365rem] border-2 w-fit" variant={"outline"}>
-                                <KeyboardIcon />
-                                Teclados em Promoção
-                            </Badge>
-                            <ProductList products={keyboards} />
-                        </div>
-                    )
-                }
-                {
-                    headphones.length > 0 && (
-                        <div className="flex flex-col gap-5">
-                            <Badge className="gap-1 text-base uppercase border-primary py-[0.365rem] border-2 w-fit" variant={"outline"}>
-                                <HeadphonesIcon />
-                                Headphones em Promoção
-                            </Badge>
-                            <ProductList products={headphones} />
-                        </div>
-                    )
-                }
-                {
-                    mousepads.length > 0 && (
-                        <div className="flex flex-col gap-5">
-                            <Badge className="gap-1 text-base uppercase border-primary py-[0.365rem] border-2 w-fit" variant={"outline"}>
-                                <MousePointerSquare />
-                                Mousepads em Promoção
-                            </Badge>
-                            <ProductList products={mousepads} />
-                        </div>
-                    )
-                }
-                {
-                    monitors.length > 0 && (
-                        <div className="flex flex-col gap-5">
-                            <Badge className="gap-1 text-base uppercase border-primary py-[0.365rem] border-2 w-fit" variant={"outline"}>
-                                <Monitor />
-                                Monitores em Promoção
-                            </Badge>
-                            <ProductList products={monitors} />
-                        </div>
-                    )
-                }
-                {
-                    speakers.length > 0 && (
-                        <div className="flex flex-col gap-5">
-                            <Badge className="gap-1 text-base uppercase border-primary py-[0.365rem] border-2 w-fit" variant={"outline"}>
-                                <SpeakerIcon />
-                                Speakers em Promoção
-                            </Badge>
-                            <ProductList products={speakers} />
-                        </div>
-                    )
-                }
+                ))}
+
             </div>
         </div>
     );
--- src/components/ui/section-tittle.tsx
+++ src/components/ui/section-tittle.tsx
@@ -1,9 +1,9 @@
 import { ComponentProps } from "react";
 
 const SectionTittle = ( {children, ...props}: ComponentProps<"p"> ) => {
+    
     return (
         <p className="font-bold uppercase mb-3" {...props}>{ children }</p>
     );
 }
-
 export default SectionTittle;
