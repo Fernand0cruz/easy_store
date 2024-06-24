d1f9570823fc663866767d50a8731be630b705f3
fernand0cruz <cruzfernando.dev@gmail.com>
2024-06-24 13:02:06 -0300

    refactor: Add margin top to footer card in UI component

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -2,7 +2,7 @@ import { Card } from "./card"
 
 const Footer = () => {
     return (
-        <Card>
+        <Card className="mt-5">
             <div className="px-4 py-4 opacity-75 max-w-screen-xl mx-auto">
                 Copyright (C) 2023-2024 <span className="font-semibold"><a href="https://fernandow.vercel.app/">Fernand0W</a></span>
             </div>
